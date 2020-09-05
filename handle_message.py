d8f2521bb89332bbf2345beeddd96a1c4fad75e4
Mark Jacobsen <Mark.Jacobsen2003@gmail.com>
2020-09-05 19:55:30 +0200

    added definitions

--- handle_message.py
+++ handle_message.py
@@ -2,7 +2,9 @@
 class for handling discord messages
 """
 import poll
-from threading import Timer
+import random
+import requests
+import json
 
 
 class MessageHandler:
@@ -22,7 +24,7 @@ class MessageHandler:
         Returns:
             None
         """
-        kick_user = clean_message[len("votekick")+1:]
+        kick_user = clean_message[len("votekick") + 1:]
         # go through all members on server
         kick_member = None
         for member in message.author.guild.members:
@@ -40,6 +42,46 @@ class MessageHandler:
         votekick_poll.successful = lambda m: m.edit(voice_channel=None)
         # start poll
         await votekick_poll.start_poll(f"Starte votekick für {kick_user}")
+
+    async def handle_insults(self, clean_message, message):
+        """
+        handle all sorts of insults
+        Args:
+            clean_message: message command without 359!
+            message: actual message object
+        Returns:
+            None
+        """
+        comeback_list = ["selber", "komm philo note junge", "halt die schnauze", "willst du stress", "komm doch",
+                         "willst du eine tracht prügel?"]
+        # detect insults
+        words = clean_message.split(" ")
+        insult_list = ["arschloch", "hurensohn", "lappen", "fick", "dumm", "dick", "spasst", "schwul", "gay",
+                       "kackspasst", "kacke"]
+        for c, word in enumerate(words):
+            if word in insult_list:
+                if c == 0 or not ("nicht" == words[c - 1] or "kein" == words[c - 1]):
+                    await message.channel.send(random.choice(comeback_list))
+
+    async def handle_definitions(self, clean_message, message):
+        """
+        defines things with google api
+        Args:
+            clean_message: cleaned up string message
+            message: message class instance
+        Returns:
+            None
+        """
+        # get word
+        word = clean_message[len("definiere") + 1:]
+        # get json
+        source = requests.get(f"https://api.dictionaryapi.dev/api/v1/entries/de/{word}").json()
+        try:
+            key = list(source[0]["meaning"].keys())[0]
+            definition = source[0]["meaning"][key][0]["definition"]
+            await message.channel.send(f"die definition von {word} ist: {definition}")
+        except:
+            await message.channel.send(f"Gibt keine definition für dein blödes Wort.")
 
     async def handle_message(self, message):
         """
@@ -65,3 +107,12 @@ class MessageHandler:
         elif clean_message.startswith("votekick"):
             await self.votekick(clean_message, message)
 
+        elif "emser depesche" in clean_message:
+            await message.channel.send("Die Emser Depesche ist ein internes Telegramm der norddeutschen Bundesregierung vom 13. Juli 1870. Darin unterrichtete der Diplomat Heinrich Abeken den norddeutschen Bundeskanzler Otto von Bismarck in Berlin über die Vorgänge in Bad Ems. Der Bundeskanzler informierte daraufhin die Presse über die Vorgänge. Diese Pressemitteilung wird zuweilen mit der eigentlichen Depesche verwechselt, weil Bismarck großteils den Wortlaut der Depesche wiederverwendete. Die Pressemitteilung führte zu Empörung in Frankreich und gilt als ein Auslöser des Deutsch-Französischen Krieges von 1870/71. Die Depesche selbst wurde nicht veröffentlicht.")
+
+        elif clean_message.startswith("definiere"):
+            await self.handle_definitions(clean_message, message)
+
+        # non commands, check for insults
+        else:
+            await self.handle_insults(clean_message, message)
