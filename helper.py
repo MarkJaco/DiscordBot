81a0b2c2f36fe9cb88be39693235bb51f558f293
Mark Jacobsen <Mark.Jacobsen2003@gmail.com>
2020-09-11 23:08:50 +0200

    added votenick

--- helper.py
+++
@@ -1,6 +0,0 @@
-"""
-helper module, that will be git ignored
-"""
-
-# bot token to modify code
-token = "NzUxNTI5Mjg3NjA1Mjg5MDcy.X1KaMA.yybTveaLA_vHDz8Q16ENQN6AL34"
---
+++ secret.py
@@ -0,0 +1,8 @@
+"""
+module won't show up on github
+
+store discord bot token here etc.
+"""
+
+
+token = "NzUxNTI5Mjg3NjA1Mjg5MDcy.X1KaMA.uiv34nZoW38zPCKrKnPswSsCeBY"
